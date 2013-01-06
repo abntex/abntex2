21a658d506d969df504941e0b87a9eb76a4428a4
laurocesar <laurocesar@gmail.com>
2013-01-06 10:51:49 -0200

    Altera??o da posi??o do arquivo README: de / para /doc/latex/abntex2, conforme estrutura can?nica do TDS; Adequa??o do build.sh para comportar a nova posi??o do arquivo README; Inclus?o de informa??es de contato no CTAN no build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 
 echo
 echo --------------------------------------------------
-echo            abnTeX2 BUILDER SCRIPT
-echo --------------------------------------------------
-echo                build zip files
-echo           deploy them on Google Code   
+echo abnTeX2 BUILDER SCRIPT
+echo http://code.google.com/p/abntex2/
+echo build zip files and deploy them on Google Code   
 echo --------------------------------------------------
 
 VERSAO=""
@@ -19,7 +18,7 @@ function build(){
 
 	# setting versions numbers
 	echo 
-	echo Building zip files version: $VERSION
+	echo Building zip files version: $VERSAO
 	echo
 
 	# setting names
@@ -32,29 +31,29 @@ function build(){
 	rm -rf target
 	mkdir -p target/abntex2/{tex,doc}
 	
-	# creating abntex2-doc.zip (only doc files)
-	zip -j $ZIP_DOC tds/doc/latex/abntex2/* -i \*.tex \*.pdf \*.bib
+	echo "$ZIP_DOC (only doc files):"
+	zip -j $ZIP_DOC tds/doc/latex/abntex2/* -i *README \*.tex \*.pdf \*.bib 
 	
-	# creating abntex2-tds.zip (tds directory structure + README)
+	echo "$ZIP_TDS (tds directory structure):"
 	cd tds
-	zip -r ../$ZIP_TDS README bibtex doc tex -i \*.tex \*.pdf \*.bib \*.bst \*.cls \*.sty README
+	zip -r ../$ZIP_TDS bibtex doc tex -i *README \*.tex \*.pdf \*.bib \*.bst \*.cls \*.sty 
 	cd ..
 	
-	# creating abntex2.zip (tex and doc browsable content + abntex2-tds.zip + README)
+	echo "$ZIP_CTAN (tex and doc browsable content + abntex2-tds.zip + README):"
 	cp tds/doc/latex/abntex2/*.tex target/abntex2/doc/
 	cp tds/doc/latex/abntex2/*.pdf target/abntex2/doc/
 	cp tds/doc/latex/abntex2/*.bib target/abntex2/doc/
 	cp tds/tex/latex/abntex2/* target/abntex2/tex/
 	cp tds/bibtex/bib/abntex2/* target/abntex2/tex/
 	cp tds/bibtex/bst/abntex2/* target/abntex2/tex/
-	cp tds/README target/abntex2
-	cp target/abntex2.tds.zip target/abntex2
+	cp tds/doc/latex/abntex2/README target/abntex2/
+	cp $ZIP_TDS target/abntex2
 	cd target
 	zip -r ../$ZIP_CTAN abntex2 -x \.DS_Store \*.gz
 	rm -rf abntex2
 	cd ..
 	
-	# creating abntex2-modelo.zip (only example files)
+	echo "$ZIP_MODELO (only example files):"
 	cd tds/doc/latex/abntex2
 	zip ../../../../$ZIP_MODELO abntex2-modelo* -i \*.pdf \*.tex \*.bib
 	cd ../../../../
@@ -87,16 +86,37 @@ function deploy(){
 	./googlecode_upload.py -s \'"$PREFIX Modelo de trabalho acadêmico com abnTeX2"\' -p abntex2 -u $1 -w $2 -l $LABELS $ZIP_MODELO
 }
 
+# ending information after build
+function printEndingInformation(){
+	echo --------------------------------------------------
+	echo Done! The following files were generated:
+	echo
+	echo $ZIP_DOC
+	echo $ZIP_TDS
+	echo $ZIP_MODELO
+	echo $ZIP_CTAN
+	echo 
+	echo Remember: you need upload $ZIP_CTAN on CTAN
+	echo at http://www.ctan.org/upload
+	echo
+	echo If you need change any information on CTAN, contact:
+	echo rainer@dante.de - Rainer Schöpf,
+	echo robin.fairbairns@cl.cam.ac.uk - Robin Fairbairns,
+	echo gene@gerd-neugebauer.de - Gerd Neugebauer and 
+	echo ctan@dante.de.
+	echo --------------------------------------------------
+}
+
 # usage information
 function printUsage(){
-	echo 
-	echo Usage:
-	echo 
-	echo  ./build.sh                   : Build zip file with no version number
-	echo  ./build.sh version           : Build zip file with version number
-	echo  ./build.sh --help            : Show this help message
-	echo  ./build.sh version user password  : Build and deploy on Google Code
 	echo
+	echo "Usage: ./build.sh ([version] [(user password)]) | [--help]"
+	echo 
+	echo "  version : Build zip file with version number"
+	echo "  user : User name on Google Code for project abntex2"
+	echo "  password : Password on Google Code for project abntex2"
+	echo "  --help : Show this help message"
+	echo 
 }
 
 
@@ -106,6 +126,7 @@ if test -z "$1"
 then
 	printUsage
 	build
+	printEndingInformation
 else
 	if [ $1 == "--help" ] ; then
 		printUsage
@@ -121,11 +142,12 @@ else
 		else
 			deploy $2 $3
 		fi
+		
+		printEndingInformation
 	fi
 fi
 
 
+
+
 # ending...
-echo --------------------------------------------------
-echo Done! 
-echo --------------------------------------------------
