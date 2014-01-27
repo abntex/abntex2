d9693ac3131e8864f8a51693a447139c08fd5407
Marcelo Santana <marcgomes@gmail.com>
2014-01-26 23:18:48 -0300

    Nova tentativa de corre??o da cria??o do arquivo TAR.GZ em sistemas Mac OS X

--- build.sh
+++ build.sh
@@ -150,7 +150,7 @@ function buildCompressed(){
 
         echo "$TAR_FILE (tds directory structure + MakeFile):"
         cd target/abntex2source
-        COPYFILES_DISABLE=1 tar cfvz ../../$TAR_FILE Makefile bibtex/bib/abntex2/*.bib bibtex/bst/abntex2/*.bst doc/latex/abntex2/{README,*.{bib,tex,pdf},examples/*.{bib,tex,pdf,jpg}} tex/latex/abntex2/*.{cls,sty}
+        tar cfvz ../../$TAR_FILE Makefile bibtex doc tex --exclude="\.*"
         cd ../..
 }
 
